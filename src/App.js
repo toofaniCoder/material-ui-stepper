50f284a2287bbfc310a34d123502c3beb1ac1bf1
toofaniCoder <smartcoder500@gmail.com>
2021-05-16 10:36:00 +0530

    add dark theme

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import LinearStepper from "./LinearStepper";
 import { CssBaseline, Container, Paper, Box } from "@material-ui/core";
-
+import { ThemeProvider, makeStyles } from "@material-ui/core/styles";
+import theme from "./theme";
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Container component={Box} p={4}>
         <Paper component={Box} p={3}>
           <LinearStepper />
         </Paper>
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 
